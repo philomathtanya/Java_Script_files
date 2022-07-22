3ff9b33bffe7067bda7254d7a2930cc06c55c779
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-07-22 21:44:47 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,21 @@
+var arr = [1, 2, 3, "tanya", "mathura"];
+arr.push("varun dhawan");
+for (let index = 0; index < arr.length; index ++) {
+    console.log(arr[index]);
+
+}
+arr.pop();
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+
+}
+arr.shift();
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+
+}
+arr.unshift("ritesh verma");
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+
+}
