6711ee8814752d177cc98073a4a7e7b8bd104bbe
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-07-26 22:30:42 +0530

    Add files via upload

---
+++ Conditional_statements.js
@@ -0,0 +1,18 @@
+console.log(1 > 2 ? console.log("true") : console.log("false"));
+var a = 10;//int
+var b = "10";//string
+if (a == b)
+{
+    console.log("it is true");
+}
+else {
+    console.log("it is false");
+}
+//checking with ===:
+if (a === b)
+{
+    console.log("it is true");
+}
+else {
+    console.log("it is false");
+}
