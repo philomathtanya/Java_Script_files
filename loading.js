0ccbd195af0cd603ce76ff762c68943d8dcb3c9e
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-07-29 20:47:10 +0530

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,11 @@
+function playloader(){
+    setTimeout(visible, 5000);
+}
+function visible(){
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container1").style.display = "block";
+    
+
+
+
+}
