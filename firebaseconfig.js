bcfa1ac63b404628b77306981c249d0df89b123e
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-07-30 21:39:01 +0530

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyADJtdf-admO8Q4WR2roYAgaL0gl7nRdsw",
+    authDomain: "crudweb-8e3d3.firebaseapp.com",
+    projectId: "crudweb-8e3d3",
+    storageBucket: "crudweb-8e3d3.appspot.com",
+    messagingSenderId: "74043022239",
+    appId: "1:74043022239:web:6a22fd653330a29a5d3baa"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
